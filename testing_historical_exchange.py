a19cf69cf80b310895544e3f801801a8fa6bbc25
sudoferraz <jedygabriel@gmail.com>
2017-10-17 04:59:01 -0200

    Added csv to usdbrl conversion

--- testing_historical_exchange.py
+++ testing_historical_exchange.py
@@ -3,4 +3,4 @@ from pandas_datareader import data, wb
 
 start_date = dt.datetime(1995, 1, 1)
 dat = data.DataReader('USDBRL=X', 'yahoo')['Adj Close']
-dat.to_csv('eurusd.csv', mode='w', header=True)
+dat.to_csv('brlusd.csv', mode='w', header=True)
