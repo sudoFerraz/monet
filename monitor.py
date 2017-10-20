ebd869ea1d3d9cdf4f4b56af60be9dddbf524c63
sudoferraz <jedygabriel@gmail.com>
2017-10-20 03:00:10 -0200

    Added updated code

--- monitor.py
+++ monitor.py
@@ -1,4 +1,22 @@
-import model.py
-import dbmodel.py
+#import model.py
+#import dbmodel.py
+import time
+import numpy as np
+import plotly as py
+import plotly.graph_objs as go
+from plotly.graph_objs import *
+import pandas_datareader.data as web
+from datetime import datetime
+from forex_python.converter import CurrencyRates
 
 
+
+while True:
+    df = web.DataReader('BRL=X', 'yahoo')
+    trace = go.Candlestick(x=df.index, open=df.Open, high=df.High, low=df.Low, close=df.Close)
+    data = [trace]
+    py.offline.plot(data, filename='Updated_historical')
+    c = CurrencyRates()
+    a =  c.get_rate('USD', 'INR')
+    print a
+    time.sleep(7200)
