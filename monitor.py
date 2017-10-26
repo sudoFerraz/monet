96e6f0aeb98260d8c85e358055efc23a2a0309bf
sudoferraz <jedygabriel@gmail.com>
2017-10-26 05:01:10 -0200

    Security

--- monitor.py
+++ monitor.py
@@ -30,28 +30,32 @@ while True:
     df = web.DataReader('BRL=X', 'yahoo')
     df.to_csv('brlusd.csv', mode='w', header=True)
     data = StockDataFrame.retype(pd.read_csv('brlusd.csv'))
-    print data['boll_ub']
-    print data['boll_lb']
-    print data
-    print data['macdh']
     close_price = data['close']
     up_bollinger = data['boll_ub']
     low_bollinger = data['boll_lb']
     last_bollinger_up = up_bollinger[-1]
-    #print last_bollinger_up[-1]
-    trace = go.Candlestick(x=data.index, open=data.open, high=data.high, low=data.low, close=data.close)
-    trace = go.Candlestick(x=data.index, close=data.macdh)
-    data = [trace]
-    py.offline.plot(data, filename='Updated_historical')
-    c = CurrencyRates()
-    a =  c.get_rate('USD', 'BRL')
-    x = np.arange(4)
-    plt.plot(up_bollinger)
-    plt.plot(low_bollinger)
-    plt.plot(close_price)
-    print up_bollinger
-    print low_bollinger
-    plt.savefig('testingstockdata.png')
-        #print a
+    last_bollinger_low = low_bollinger[-1]
+    last_close_price = close_price[-1]
     print last_bollinger_up
+    print last_bollinger_low
+    print last_close_price
+    #print data['macdh']
+    #print data['boll_ub']
+    #print data
+    #print data['boll_lb']
+    #print last_bollinger_up[-1]
+    #trace = go.Candlestick(x=data.index, open=data.open, high=data.high, low=data.low, close=data.close)
+    #trace = go.Candlestick(x=data.index, close=data.macdh)
+    #data = [trace]
+    #py.offline.plot(data, filename='Updated_historical')
+    #c = CurrencyRates()
+    #a =  c.get_rate('USD', 'BRL')
+    #x = np.arange(4)
+    #plt.plot(up_bollinger)
+    #plt.plot(low_bollinger)
+    #plt.plot(close_price)
+    #print up_bollinger
+    #print low_bollinger
+    #plt.savefig('testingstockdata.png')
+        #print a
     time.sleep(60)
